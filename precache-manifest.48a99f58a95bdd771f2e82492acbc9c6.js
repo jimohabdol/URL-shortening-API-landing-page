00a19319a42d3f96b9c49be9c8e315fece0430e9
Jimoh Abdulrahman <39130903+abdol4real@users.noreply.github.com>
2020-03-02 16:54:56 +0100

    Updates

---
+++ precache-manifest.48a99f58a95bdd771f2e82492acbc9c6.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b621a9cdeb97e30ae58aeb7a203b41d",
+    "url": "/URL-shortening-API-landing-page/index.html"
+  },
+  {
+    "revision": "75cd3fa5dbe8b4626fa5",
+    "url": "/URL-shortening-API-landing-page/static/css/main.cc3c6c35.chunk.css"
+  },
+  {
+    "revision": "b875fc26607f0b37d14f",
+    "url": "/URL-shortening-API-landing-page/static/js/2.0395d8e1.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/URL-shortening-API-landing-page/static/js/2.0395d8e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75cd3fa5dbe8b4626fa5",
+    "url": "/URL-shortening-API-landing-page/static/js/main.1811f70d.chunk.js"
+  },
+  {
+    "revision": "ee3276ab0f9a2f570048",
+    "url": "/URL-shortening-API-landing-page/static/js/runtime-main.99906ae1.js"
+  },
+  {
+    "revision": "4a7a7f615fb37565a0a706a7379dfb7e",
+    "url": "/URL-shortening-API-landing-page/static/media/bg-shorten-mobile.4a7a7f61.svg"
+  },
+  {
+    "revision": "1922b68a4d1dce29b2c40904cdbc1bba",
+    "url": "/URL-shortening-API-landing-page/static/media/icon-brand-recognition.1922b68a.svg"
+  },
+  {
+    "revision": "e3d9caa9df26426d598106f7f7289625",
+    "url": "/URL-shortening-API-landing-page/static/media/icon-detailed-records.e3d9caa9.svg"
+  },
+  {
+    "revision": "7febcff9f143b83553d544fc04fc5b09",
+    "url": "/URL-shortening-API-landing-page/static/media/icon-facebook.7febcff9.svg"
+  },
+  {
+    "revision": "cc099cea0da2f5ae13aef182c107a854",
+    "url": "/URL-shortening-API-landing-page/static/media/icon-fully-customizable.cc099cea.svg"
+  },
+  {
+    "revision": "996b697c33ab8772e59e3c9bec1124ea",
+    "url": "/URL-shortening-API-landing-page/static/media/icon-instagram.996b697c.svg"
+  },
+  {
+    "revision": "c557c3d4e80b5eda1ae120c8c2257e26",
+    "url": "/URL-shortening-API-landing-page/static/media/icon-pinterest.c557c3d4.svg"
+  },
+  {
+    "revision": "cb4fea0b4771a5dfc70f787d5e01c91e",
+    "url": "/URL-shortening-API-landing-page/static/media/icon-twitter.cb4fea0b.svg"
+  },
+  {
+    "revision": "5029d0493c803aac6c96e13be419ecb7",
+    "url": "/URL-shortening-API-landing-page/static/media/illustration-working.5029d049.svg"
+  },
+  {
+    "revision": "6387840f9b6e8720165db8c73185bc51",
+    "url": "/URL-shortening-API-landing-page/static/media/logo.6387840f.svg"
+  }
+]);
